f682352fb1d1dee3c61bc5739d335c0f1023769e
DEVVRATSHARMA-ZZZ <devzzzwebhosting@gmail.com>
2025-04-23 00:04:13 +0530

    new commit

--- addSchool.js
+++ addSchool.js
@@ -4,28 +4,34 @@ const db = require("./dbConnection");
 router.post("/", (req, res) => {
   const { name, address, latitude, longitude } = req.body;
   if (!name || !address || !latitude || !longitude) {
-    return res.status(400).send("All fields are required");
+     res.status(400).send("All fields are required");
   }
   if (typeof name !== "string") {
-    return res.status(400).send("Name  must be strings");
+     res.status(400).send("Name  must be strings");
+     return;
   }
   if (typeof address !== "string") {
-    return res.status(400).send(" address must be strings");
+     res.status(400).send(" address must be strings");
+     return;
   }
   if (typeof latitude !== "number") {
-    return res.status(400).send("Latitude must be float values");
+     res.status(400).send("Latitude must be float values");
+     return;
   }
   if (typeof longitude !== "number") {
-    return res.status(400).send(" longitude must be float values");
+     res.status(400).send(" longitude must be float values");
+     return;
   }
   const checkQuery =
     "SELECT * FROM schools WHERE latitude = ? AND longitude = ?";
   db.query(checkQuery, [latitude, longitude], (err, results) => {
     if (err) {
-      return res.status(500).send(`Error checking for existing school: ${err}`);
+       res.status(500).send(`Error checking for existing school: ${err}`);
+       return;
     }
     if (results.length > 0) {
-      return res.status(400).send("School already exists at this location");
+       res.status(400).send("School already exists at this location");
+       return;
     }
   });
 
@@ -33,7 +39,8 @@ router.post("/", (req, res) => {
     "INSERT INTO schools(name,address,latitude,longitude) VALUES(?,?,?,?)";
   db.query(querry, [name, address, latitude, longitude], (err, result) => {
     if (err) {
-      return res.status(500).send(`Inserting error :${err} `);
+       res.status(500).send(`Inserting error :${err} `);
+       return;
     } else {
       res.status(200).send(`school added succesfully`);
       console.log(`school added succesfully`);
