e625f713ebf01054a4b9184d188f6567a8dc5e77
DEVVRATSHARMA-ZZZ <devzzzwebhosting@gmail.com>
2025-04-22 23:25:43 +0530

    new commit

--- dbConnection.js
+++ dbConnection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const db = mysql.createConnection(process.env.Database_URL);
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
