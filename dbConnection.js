757d23f3322b48d02f76979c442d3cae2f2b3b14
DEVVRATSHARMA-ZZZ <devzzzwebhosting@gmail.com>
2025-04-22 23:14:01 +0530

    change in the .env file

--- dbConnection.js
+++ dbConnection.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const db = mysql.createConnection(process.env.DATABASE_URL);
- 
-  db.connect((err) => {
-    if (err) {
-      console.error("Database connection failed:", err);
-      return;
-    }
-    console.log(" MySQL connected Sucessfully");
-  });
-  module.exports = db;
+const db = mysql.createConnection(process.env.Database_URL);
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log(" MySQL connected Sucessfully");
+});
+module.exports = db;
